490c732e8490130aee0a9136b8c5a74a9e97b9af
Anthony Tan <tony.face2ai@gmail.com>
2024-11-11 17:26:03 +0800

    dueling dqn update

--- agents/dueling_dqn_agent.py
+++ agents/dueling_dqn_agent.py
@@ -4,23 +4,22 @@ from models.dqn_networks import DuelingDQNAtari
 
 class DuelingDQNValueFunction(DQNValueFunction):
     def __init__(self, input_channel: int, action_dim: int, learning_rate: float,
-                 gamma: float, step_c: int, model_saving_period: int, device: torch.device, logger: Logger):
+                 gamma: float, model_save_path: str, device: torch.device, logger: Logger):
         super(DuelingDQNValueFunction, self).__init__(input_channel, action_dim, learning_rate,
-                                                      gamma, step_c, model_saving_period, device, logger)
+                                                      gamma, model_save_path, device, logger)
         self.value_nn = DuelingDQNAtari(input_channel, action_dim).to(device)
         self.target_value_nn = DuelingDQNAtari(input_channel, action_dim).to(device)
-        self.optimizer = torch.optim.Adam(self.value_nn.parameters(), lr=learning_rate)
 
 
 class DuelingDQNAgent(DQNAgent):
     def __init__(self, screen_size: int, action_space,
-                 mini_batch_size: int, replay_buffer_size: int, min_update_sample_size: int,
-                 learning_rate: float, step_c: int, model_saving_period: int,
+                 mini_batch_size: int, replay_buffer_size: int, replay_start_size: int,
+                 learning_rate: float, step_c: int,
                  gamma: float, training_episodes: int, phi_channel: int, epsilon_max: float, epsilon_min: float,
-                 exploration_steps: int, device: torch.device, logger: Logger):
+                 exploration_steps: int, device: torch.device, exp_path: str, exp_name: str,  logger: Logger):
         super(DuelingDQNAgent, self).__init__(screen_size, action_space, mini_batch_size,
-                                              replay_buffer_size, min_update_sample_size, learning_rate, step_c,
-                                              model_saving_period, gamma, training_episodes, phi_channel, epsilon_max,
-                                              epsilon_min, exploration_steps, device, logger)
+                                              replay_buffer_size, replay_start_size, learning_rate, step_c,
+                                              gamma, training_episodes, phi_channel, epsilon_max,
+                                              epsilon_min, exploration_steps, device, exp_path, exp_name, logger)
         self.value_function = DuelingDQNValueFunction(phi_channel, action_space.n, learning_rate,
-                                                      gamma, step_c, model_saving_period, device, logger)
+                                                      gamma, os.path.join(exp_path, exp_name), device, logger)
