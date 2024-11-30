8da76241098671e7b572187c0e8eecc0cb646cbb
Anthony <ton.face2ai@gmail.com>
2024-11-30 19:58:40 +0800

    dueling dqn and proportional priority dqn algorithm update

--- agents/dqn_pp_agent.py
+++ agents/dqn_pp_agent.py
@@ -1,25 +1,30 @@
 import gc
+
+import torch
+
 from agents.dqn_agent import *
 
 
 class ProportionalPrioritization(DQNExperienceReplay):
-    def __init__(self, capacity: int, phi_channel: int, alpha: float = 0.6, beta: float = 0.4):
+    def __init__(self, capacity: int, phi_channel: int, device: torch.device, alpha: float = 0.6, beta: float = 0.4):
         super(ProportionalPrioritization, self).__init__(capacity, phi_channel)
-        self.p = np.zeros(capacity)
+
         self.alpha = alpha
         self.beta = beta
+        self.device = device
+        self.p = torch.zeros(capacity, dtype=torch.float32).to(self.device, dtype=torch.float32)
 
     def store(self, observation: np.ndarray, action: np.ndarray, reward: np.ndarray,
               next_observation: np.ndarray, done: np.ndarray, truncated: np.ndarray):
         super(ProportionalPrioritization, self).store(observation, action, reward, next_observation, done, truncated)
         p_position = self.position - 1
-        self.p[p_position] = np.max(self.p) if self.__len__() > 1 else 1.0
+        self.p[p_position] = torch.max(self.p) if self.__len__() > 1 else 1.0
 
-    def sample(self, batch_size: int ):
+    def sample(self, batch_size: int):
         n = self.__len__()
         # normalize the p as a probability distribution
         p_alpha = self.p[:n]**self.alpha
-        p = p_alpha / np.sum(p_alpha)
+        p = np.array(p_alpha.cpu() / torch.sum(p_alpha).cpu())
         # select the index of the samples
         idx = np.random.choice(np.arange(n), batch_size, p=p, replace=False)
         w = (n * p) ** -self.beta
@@ -63,7 +68,7 @@ class DQNPPAgent(DQNAgent):
                                          epsilon_min, exploration_steps, device, exp_path, exp_name, logger)
         del self.memory
         gc.collect()
-        self.memory = ProportionalPrioritization(replay_buffer_size,phi_channel, alpha, beta)
+        self.memory = ProportionalPrioritization(replay_buffer_size,phi_channel, device, alpha, beta)
 
     def train_one_step(self):
         """
@@ -72,7 +77,8 @@ class DQNPPAgent(DQNAgent):
         if len(self.memory) > self.replay_start_size:
             samples, w, idx = self.memory.sample(self.mini_batch_size)
             loss = self.value_function.update(samples, w)
-            self.memory.p[idx] = loss.reshape(1, -1) + np.float32(1e-5)
+            self.memory.p[idx] = (torch.as_tensor(loss, dtype=torch.float32).reshape(1, -1) +
+                                  torch.tensor(1e-5, dtype=torch.float32))
             self.update_step += 1
             self.loss_log.append(np.mean(loss))
             if self.update_step % self.step_c == 0:
